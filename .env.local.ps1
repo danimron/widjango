57c34f91ecea203d945e66d91eadfbefae800ce9
Okka Riswana <okka.riswana@pm.me>
2019-11-27 13:47:37 +0700

    Removed Env Var quotes

--- .env.local.ps1
+++ .env.local.ps1
@@ -1,5 +1,5 @@
-$env:DEBUG="1"
-$env:SECRET_KEY="rusiah"
-$env:DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]"
-$env:DATABASE="sqlite"
-$env:SQL_ENGINE="django.db.backends.sqlite3"
+$env:DEBUG=1
+$env:SECRET_KEY=rusiah
+$env:DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]
+$env:DATABASE=sqlite
+$env:SQL_ENGINE=django.db.backends.sqlite3
